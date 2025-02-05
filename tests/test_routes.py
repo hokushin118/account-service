665a239bb86bfbd78a085250aec4168cf5f19bf0
V A <cbotee@proton.me>
2025-02-05 19:48:47 +0300

    created get account by id endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -136,3 +136,19 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_get_account_by_id(self):
+        """It should Read a single Account."""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{ACCOUNT_ENDPOINT}/{account.id}",
+            content_type='application/json'
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data['name'], account.name)
+
+    def test_get_account_by_id_not_found(self):
+        """It should not Read an Account that is not found."""
+        resp = self.client.get(f"{ACCOUNT_ENDPOINT}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
