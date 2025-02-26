adfb7935f7340e894dce76faf3b040b22f392a92
V A <cbotee@proton.me>
2025-02-26 19:11:46 +0300

    added migration revert pipeline

--- service/common/cli_commands.py
+++ service/common/cli_commands.py
@@ -17,6 +17,8 @@ logger = logging.getLogger(__name__)
 # This class contains all the functionality of the extension
 migrate = Migrate(app, db)
 
+logger.debug("Migration directory: %s", migrate.directory)
+
 
 ######################################################################
 # Command to initialize migration
@@ -172,38 +174,92 @@ def db_upgrade() -> None:
 # Command to revert database migration
 # Usage:
 #   flask db-downgrade
-#   flask db-downgrade 2
+#   flask db-downgrade -2
+#   flask db-downgrade 1234abcd
 ######################################################################
 @app.cli.command('db-downgrade')
 @click.argument(
-    'steps',
-    type=int,
-    default=1
+    'revision',
+    type=str,
+    default='-1'  # Default to one step back
 )
-def db_downgrade(steps: int) -> None:
+def db_downgrade(revision: str) -> None:
     """Downgrades the database to a previous migration.
 
     This command reverts the database schema to an earlier state by applying
-    previous migration scripts in reverse order.  You specify the number of
-    steps to downgrade.
+    previous migration scripts. You can specify either the number of steps to
+    downgrade (e.g., -2) or the specific revision ID to downgrade to.
 
     Args:
-        steps (int): The number of migration steps to downgrade.  This must be a
-            positive integer.
+        revision (str): The revision to downgrade to. This can be either:
+            - A negative integer (e.g., -2) representing the number of steps to
+              downgrade.
+            - A specific revision ID (e.g., '1234abcd').
 
     Usage:
         flask db-downgrade
-        flask db-downgrade <number_of_steps>  (e.g., flask db-downgrade 2)
+        flask db-downgrade <number_of_steps>  (e.g., flask db-downgrade -2)
+        flask db-downgrade <revision_id> (e.g., flask db-downgrade 1234abcd)
 
     Returns:
         None
     """
     import flask_migrate  # pylint: disable=E0401, C0415
 
-    logger.debug("Downgrading %s steps.", steps)
+    logger.debug("Downgrading to revision: %s.", revision)
 
     with app.app_context():
-        flask_migrate.downgrade(revision=f'-{steps}')
+        flask_migrate.downgrade(revision=revision)
+
+
+######################################################################
+# Command to display the history of database migrations
+# Usage:
+#   flask db-history
+#   flask db-history -v
+#   flask db-history --verbose
+#   flask db-history --rev-range 1234abcd:5678efgh
+######################################################################
+@app.cli.command('db-history')
+@click.option(
+    '-v', '--verbose',
+    is_flag=True,
+    help='Display verbose output.'
+)
+@click.option(
+    '--rev-range',
+    help='Revision range to display (e.g., "base:head" or "1234abcd:5678efgh").'
+)
+def db_history(verbose: bool, rev_range: str) -> None:
+    """This command will display the history of database migrations, including:
+
+        Revision IDs: The unique identifiers for each migration.
+        Creation Dates: The dates and times when the migrations were created.
+        Migration Messages: The messages associated with each migration.
+
+    Args:
+        verbose (bool): If True, displays verbose output.
+        rev_range (str): A revision range to filter the history.
+
+    Usage:
+        flask db-history
+        flask db-history -v
+        flask db-history --verbose
+        flask db-history --rev-range 1234abcd:5678efgh
+
+    Returns:
+        None
+    """
+    import flask_migrate  # pylint: disable=E0401, C0415
+
+    logger.debug(
+        "Displaying migration history (verbose=%s, rev_range=%s)",
+        verbose,
+        rev_range
+    )
+
+    with app.app_context():
+        flask_migrate.history(verbose=verbose, rev_range=rev_range)
 
 
 ######################################################################
